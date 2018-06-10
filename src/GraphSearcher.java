b97df9a6e2f16352a9b0aed7f87267bb49222d37
tn16jv <tn16jv@brocku.ca>
2018-06-10 12:24:23 -0400

    Some very minor reviews of the GraphSearcher

--- src/GraphSearcher.java
+++ src/GraphSearcher.java
@@ -5,7 +5,8 @@ import static java.lang.Math.sqrt;
 /**
  * Author's Name: ThaiBinh Nguyen
  *
- * Implementation of a basic Travelling Salesman Problem algorithm that is extendable to multithreading.
+ * Implementation of a genetic algorithm for approximating a solution to the Travelling Salesman Problem.
+ * Random paths are generated and the best one (shortest length) is kept at the end of each search.
  */
 public class GraphSearcher implements Runnable {
     Semaphore globalAccess;
